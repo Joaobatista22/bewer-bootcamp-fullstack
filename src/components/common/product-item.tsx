72b3426111ac790709ee211039f64b58edf5ad48
Joaobatista22 <joaobatiista.ds@gmail.com>
2025-08-09 23:53:30 -0300

    feat:add category products page

--- src/components/common/product-item.tsx
+++ src/components/common/product-item.tsx
@@ -3,14 +3,16 @@ import Link from "next/link";
 
 import { productTable, productVariantTable } from "@/db/schema";
 import { formatCentsToBRL } from "@/helpers/money";
+import { cn } from "@/lib/utils";
 
 interface ProductItemProps {
   product: typeof productTable.$inferSelect & {
     variants: (typeof productVariantTable.$inferSelect)[];
   };
+  textContainerClassName?: string;
 }
 
-const ProductItem = ({ product }: ProductItemProps) => {
+const ProductItem = ({ product, textContainerClassName }: ProductItemProps) => {
   // Garantir que existe pelo menos uma variante
   const firstVariant = product.variants?.[0];
   if (!firstVariant) {
@@ -30,11 +32,12 @@ const ProductItem = ({ product }: ProductItemProps) => {
       <Image
         src={imageUrl}
         alt={`${product.name} - ${firstVariant.name}`}
-        width={200}
-        height={200}
-        className="rounded-3xl object-cover"
+        sizes="100vw"
+        height={0}
+        width={0}
+        className="h-auto w-full rounded-3xl"
       />
-      <div className="flex max-w-[200px] flex-col gap-1">
+      <div className={cn("flex max-w-[200px] flex-col gap-1", textContainerClassName = "max-w-full")}>
         <p className="truncate text-sm font-medium">{product.name}</p>
         <p className="text-muted-foreground truncate text-xs font-medium">
           {product.description}
